5cc4eea1c383c1b9d80c2d6bf97158c8fdf416fa
Elena Vonder Meulen <evonder13@gmail.com>
2020-09-30 11:58:02 -0400

    Add project files.

---
+++ OOP/Quad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Quad
+    {
+        public int Side1 { get; set; }
+        public int Side2 { get; set; }
+        public int Side3 { get; set; }
+        public int Side4 { get; set; }
+
+        public Quad()
+        {
+
+        }
+    }
+}
